8c0c35c0017788b5ae688e79b2513d3b432287f7
Tanut-M2 <tanat44@gmail.com>
2023-03-21 21:59:09 +0100

    add files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "public" }],
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
